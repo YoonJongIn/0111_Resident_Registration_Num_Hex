39830109d43603055c12ad0a18998c4053fcb7ce
JongIn Yoon <bellpp93@gmail.com>
2022-01-11 17:53:18 +0900

    0111_Jumin_Hex

---
+++ src/JuminCheckEx.java
@@ -0,0 +1,19 @@
+
+public class JuminCheckEx {
+
+	public static void main(String[] args) {
+		//준비 단계
+		String juminNum;
+		int sum = 0;
+		
+		//입력 단계
+		
+		
+		//처리 단계
+		
+		
+		//출력 단계
+		
+	}
+
+}
